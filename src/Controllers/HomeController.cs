80ef7f6d9ca3258b790bcafb5478488133e00224
Rob Reagan <rob@textrequest.com>
2020-03-16 16:25:01 -0400

    Initial commit

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PlusPlus.Models;
+
+namespace PlusPlus.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private static int _numberOfTimesClicked = 0;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            IncrementViewModel model = new IncrementViewModel();
+            model.NumberOfTimesClicked = _numberOfTimesClicked; 
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Increment()
+        {
+            _numberOfTimesClicked++;
+            IncrementViewModel model = new IncrementViewModel();
+            model.NumberOfTimesClicked = _numberOfTimesClicked;
+            return View("Index", model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PlusPlus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
